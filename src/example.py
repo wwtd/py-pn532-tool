ceee4eb4da42cb70e290888c273cf7050ad2f59b
wwtd <51043256+wwtd@users.noreply.github.com>
2025-04-14 23:01:36 +0800

    read UID

--- src/example.py
+++ src/example.py
@@ -4,15 +4,13 @@ if __name__ == '__main__':
     try:
         pn532 = PN532('/dev/ttyUSB0')
 
-        pn532.GetFirmwareVersion()
-        pn532.InListPassiveTarget()
-
-        pn532.GetGeneralStatus()
-
-        pn532.FielfOff()
-        pn532.GetGeneralStatus()
-        pn532.FieldOn()
-        pn532.GetGeneralStatus()
+        ListPassiveTarget_resp = pn532.InListPassiveTarget()
+        print(f"NbTg : {ListPassiveTarget_resp[7]}")
+        print(f"Tg : {ListPassiveTarget_resp[8]}")
+        print(f"SENS_RES : {hex(ListPassiveTarget_resp[10])} {hex(ListPassiveTarget_resp[9])} ")
+        print(f"SEL_RES : {hex(ListPassiveTarget_resp[11])}")
+        print(f"NFCIDLength : {hex(ListPassiveTarget_resp[12])}")
+        print(f"NFCID : {hex(ListPassiveTarget_resp[13])} {hex(ListPassiveTarget_resp[14])} {hex(ListPassiveTarget_resp[15])} {hex(ListPassiveTarget_resp[16])}")
 
         pn532.close()
 
